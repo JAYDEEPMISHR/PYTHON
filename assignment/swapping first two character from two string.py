83f66346aa8b033122150b6bca8e5946279183ed
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2023-03-29 09:18:05 +0530

    ASSIGNMENT

---
+++ assignment/swapping first two character from two string.py
@@ -0,0 +1,9 @@
+# Take two strings
+a = 'Jaydeep'
+b = 'Mishra'
+print("Before Swap :",a," ",b)
+
+# Do slicing and addition of two new strings
+a1 = b[:2] + a[2:]
+b1 = a[:2] + b[2:]
+print("After Swap :",a1," ",b1)

c6e1068ede70a795170d5ad15e493b34b1c2340b
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2023-03-31 15:30:27 +0530

    FUNDAMENTALS OF PYTHON

---
+++ assignment/ADD ING AND LY TO THE STRING.py
@@ -0,0 +1,15 @@
+# Python program to add 'ing' at the end of a given string(length should be at least 3). If the given string already ends with 'ing'then add 'ly' instead. If the string length of the given string is lessthan 3, leave it unchanged.
+
+my_string = input("enter a string: ")
+
+
+if len(my_string) < 3:
+    new_string = my_string
+    
+elif my_string[-3:] == 'ing':
+    new_string = my_string + 'ly'
+    
+else:
+    new_string = my_string + 'ing'
+    
+print (new_string)

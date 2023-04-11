7700c16823192272130b7b06c93ef034a6092266
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2023-04-11 22:44:03 +0530

    Advance python programming

--- assignment/Advance python programming/Python program to copy the contents of a file to another file.py
+++ assignment/Advance python programming/Python program to copy the contents of a file to another file.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Python program to copy the contents of a file to another file.
 
 with open("program 1.txt",'r') as file:
@@ -6,3 +7,13 @@ with open("program 1.txt",'r') as file:
             file1.write(line)
 
 
+=======
+# Python program to copy the contents of a file to another file.
+
+with open("program 1.txt",'r') as file:
+    with open("program 2.txt",'w') as file1:
+        for line in file:
+            file1.write(line)
+
+
+>>>>>>> 763f7713ac1725ba7d2beafc214de1da0bafd124
